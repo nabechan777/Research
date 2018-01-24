b8beb50226c70eafa43f8604be6b95d6f20c296d
nabechan777 <i.likesusi.d@gmail.com>
2018-01-25 00:09:01 +0900

    separate function

--- app/Main.hs
+++ app/Main.hs
@@ -3,27 +3,28 @@ module Main
     ( main
     )where
 
-import Control.Monad
-import Control.Applicative
-import Graphics.UI.WX hiding (Event)
-import Reactive.Banana
-import Reactive.Banana.WX
-import Library.AccessDatabase
-import Library.Interface
-import Database.Relations.Student as Student
-import Database.Relations.Lecture as Lecture
-import Database.Relations.Course as Course
-import Database.Relations.Grade as Grade
+import           Control.Applicative
+import           Control.Monad
+import           Database.Relations.Course  as C
+import           Database.Relations.Grade   as G
+import           Database.Relations.Lecture as L
+import           Database.Relations.Student as S
+import           Graphics.UI.WX             hiding (Event)
+import           Library.AccessDatabase
+import           Library.Interface
+import           Reactive.Banana
+import           Reactive.Banana.WX
 
 -- | PostgreSQLサーバから学生, 講義の情報を取り出し、インターフェースを構築する。
 main :: IO ()
 main = handleSqlError' $ withConnectionIO (connectPostgreSQL "dbname=research") $ \conn -> start $ do
     student        <- runRelation conn selectAllFromStudentWhereStudentNumber 12345
-    lectures       <- sequence $ map (\x -> runRelation conn selectAllFromLectureWherePeriod x) period
-    grades <- runRelation conn selectAllFromGradeWhereStudentId $ Student.studentId (head student)
+    lectures       <- mapM (runRelation conn selectAllFromLectureWherePeriod) period
+    grades <- runRelation conn selectAllFromGradeWhereStudentId $ S.studentId (head student)
 
     f              <- frame [text := "履修登録"]
-    choices        <- sequence $ map (\l -> choice f [items := ("--" : map Lecture.name l)]) lectures
+    -- choices        <- mapM (\l -> choice f [items := ("--" : map L.name l)]) lectures
+    choices <- createChoices f lectures
     beforeCommonValue <- staticText f [text := (show . common . head) $ grades]
     beforeSpecilizedValue <- staticText f [text := (show . special . head) $ grades]
     afterCommonValue    <- staticText f [text := (show . common . head) $ grades]
@@ -32,14 +33,7 @@ main = handleSqlError' $ withConnectionIO (connectPostgreSQL "dbname=research") 
     Graphics.UI.WX.set f [layout := column 10 [
                              createSemesterInformation "秋"
                             ,createStudentInformation $ head student
-                            ,boxed "履修" $ grid 5 5 [
-                                 map label ["", "月", "火", "水", "木", "金", "土"]
-                                ,label "1" : take 6 (map widget choices)
-                                ,label "2" : take 6 (drop 6  (map widget choices))
-                                ,label "3" : take 6 (drop 12 (map widget choices))
-                                ,label "4" : take 6 (drop 18 (map widget choices))
-                                ,label "5" : take 6 (drop 24 (map widget choices))
-                            ]
+                            ,createRegistrationBoxed choices
                             ,boxed "成績" $ column 10 [
                                 row 5 [label "共通: ", widget beforeCommonValue, label " -> ", widget afterCommonValue],
                                 row 5 [label "専門: ", widget beforeSpecilizedValue, label " -> ", widget afterSpecilizedValue]
@@ -47,8 +41,8 @@ main = handleSqlError' $ withConnectionIO (connectPostgreSQL "dbname=research") 
 
     let netWorkDescription :: MomentIO ()
         netWorkDescription = do
-            echoices <- mapM (\c -> event0 c select) choices :: MomentIO [Event ()]
-            bchoices <- mapM (\c -> behavior c selection) choices :: MomentIO [Behavior Int]
+            echoices <- mapM (`event0` select) choices
+            bchoices <- mapM (`behavior` selection) choices
             bcommon <- behavior beforeCommonValue text
             bspecilized <- behavior beforeSpecilizedValue text
 
@@ -58,7 +52,7 @@ main = handleSqlError' $ withConnectionIO (connectPostgreSQL "dbname=research") 
                 fetchLecture ls c = Just (ls !! (c - 1))
 
                 fetchCredit :: String -> Maybe Lecture -> Int
-                fetchCredit s ml = maybe 0 tmp ml
+                fetchCredit s = maybe 0 tmp
                     where
                         tmp :: Lecture -> Int
                         tmp l = if field l == s then fromIntegral . credit $ l else 0
@@ -66,13 +60,12 @@ main = handleSqlError' $ withConnectionIO (connectPostgreSQL "dbname=research") 
                 result :: String -> Behavior Int
                 result z = result' behaviorCommonAndSpecilized
                     where
-                        bchoices' = zipWith (\b e -> imposeChanges b e) bchoices echoices
+                        bchoices' = zipWith imposeChanges bchoices echoices
 
                         behaviorCommonAndSpecilized :: Behavior String
                         behaviorCommonAndSpecilized = case z of
                             "共通" -> bcommon
                             "専門" -> bspecilized
-                            otherwise -> undefined
 
                         result' :: Behavior String -> Behavior Int
                         result' o = foldr (\a b -> (+) <$> a <*> b) (read <$> o) bs'
--- src/Library/Interface.hs
+++ src/Library/Interface.hs
@@ -1,14 +1,14 @@
 module Library.Interface
     ( createSemesterInformation
     , createStudentInformation
+    , createChoices
+    , createRegistrationBoxed
     )where
 
-import Control.Applicative
-import Graphics.UI.WX
-import qualified Database.Relations.Student as Student
-import Database.Relations.Student
-import qualified Database.Relations.Lecture as Lecture
-import Database.Relations.Lecture
+import           Control.Applicative
+import           Database.Relations.Lecture as L
+import           Database.Relations.Student as S
+import           Graphics.UI.WX
 
 
 type Semester = String
@@ -19,36 +19,34 @@ createStudentInformation :: Student -> Layout
 createStudentInformation student = boxed "学生" $ grid 5 5 $ zipWith (\name value -> [label name, value]) atributeNames atributeValues
     where
         atributeNames = ["学生証番号:", "氏名:", "学部:", "学科:", "セメスター:"]
-        atributeValues = map label $ map (\f -> f student) [show . studentNumber, Student.name, faculty, department, semester]
+        atributeValues = map (label . (\f -> f student)) [show . studentNumber, S.name, faculty, department, semester]
 {-
     f: Frame
     wl: 各曜日の各時限の講義のリスト
     履修登録のレイアウトを生成する。
 -}
 type OneWeekLecture = [[Lecture]] -- [["オペレーティングシステム", "心理学の世界"], ["微分積分1", "線形代数1"], ...]
-createCourseRegistration :: Frame () -> OneWeekLecture -> IO (Layout)
-createCourseRegistration f wl =  do
-    choices <- sequence $ map (\l -> fmap widget (choice f [items := ("--" : (map Lecture.name l))])) wl
-    -- 各曜日のx限目の授業
-    let takeSix = take 6
-        choices_1 = (period !! 0) : (takeSix choices)
-        choices_2 = (period !! 1) : (takeSix $ drop 6 choices)
-        choices_3 = (period !! 2) : (takeSix $ drop 12 choices)
-        choices_4 = (period !! 3) : (takeSix $ drop 18 choices)
-        choices_5 = (period !! 4) : (takeSix $ drop 24 choices)
-    return $ boxed "履修" (grid 5 5 [weeksLabel, choices_1, choices_2, choices_3, choices_4, choices_5])
+createChoices :: Frame () -> OneWeekLecture -> IO [Choice ()]
+createChoices f xs = mapM (\x -> choice f [items := "--" : map L.name x]) xs
+
+createRegistrationBoxed :: [Choice ()] -> Layout
+createRegistrationBoxed xs = boxed "履修" $ grid 5 5 $ (tmp3 . tmp2 . tmp1) $ map widget xs
     where
-        period = map (label . show) [1, 2, 3, 4, 5]
-        weeksLabel = map label ["", "月", "火", "水", "木", "金", "土"]
+        tmp1 :: [Layout] -> [[Layout]]
+        tmp1 [] = []
+        tmp1 xs = take 6 xs : tmp1 (drop 6 xs)
 
+        tmp2 :: [[Layout]] -> [[Layout]]
+        tmp2 = zipWith (:) $ map label ["1", "2", "3", "4", "5"]
 
-type Specilize = Int
+        tmp3 = (map label ["", "月", "火", "水", "木", "金", "土"] :)
+
+type Specilized = Int
 type Common = Int
-createRecord :: Frame () -> (Common, Specilize) -> IO (Layout)
-createRecord f (c, s) = do
-    commonValue <- fmap widget $ staticText f [text := show c]
-    specilizeValue <- fmap widget $ staticText f [text := show s]
-    return $ boxed "成績" $ row 10 [row 5 [commonLabel, commonValue], row 5 [specilizeLabel, specilizeValue]]
-    where
-        commonLabel = label "共通:"
-        specilizeLabel = label "専門:"
+createStaticTextPair :: Frame () -> (Common, Specilized) -> IO (StaticText (), StaticText ())
+createStaticTextPair f (c, s) = undefined
+
+type BeforeRecord = (StaticText (), StaticText ())
+type AfterRecord = (StaticText (), StaticText ())
+createRecordBoxed ::  BeforeRecord -> AfterRecord -> IO Layout
+createRecordBoxed = undefined
